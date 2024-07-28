5c862163581c1b0fa99123d9e6a2af8b2ce4be12
satnam379 <satnam@gmail.com>
2024-07-28 18:35:49 +0530

    changes for android configuration

--- services/services.js
+++ services/services.js
@@ -1,15 +1,15 @@
-
 import axios from 'axios';
 
-const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWZmOTE1OTdhZTZiMDU3MWViYzAxMGQ1YTJiZDUyZSIsIm5iZiI6MTcyMTkzMzkxNC40NzA2OTcsInN1YiI6IjY2YTI1NzYzY2EyMzA4N2I0YWNmODdmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3aq5YafhgnqyLg0JId3gSpaVKVOVuZp8zw0DajHEpbw';
+const API_KEY =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZWZmOTE1OTdhZTZiMDU3MWViYzAxMGQ1YTJiZDUyZSIsIm5iZiI6MTcyMTkzMzkxNC40NzA2OTcsInN1YiI6IjY2YTI1NzYzY2EyMzA4N2I0YWNmODdmMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.3aq5YafhgnqyLg0JId3gSpaVKVOVuZp8zw0DajHEpbw';
 const BASE_URL = 'https://api.themoviedb.org/3';
 
 const api = axios.create({
   baseURL: BASE_URL,
 });
 
-export const getMovies = async (category,page) => {
-  const response = await api.get(`/movie/${category}?page=${page}`,{
+export const getMovies = async (category, page) => {
+  const response = await api.get(`/movie/${category}?page=${page}`, {
     headers: {
       Authorization: `Bearer ${API_KEY}`,
     },
@@ -17,8 +17,8 @@ export const getMovies = async (category,page) => {
   return response.data;
 };
 
-export const getMovieDetail = async (category) => {
-  const response = await api.get(`/movie/${category}`,{
+export const getMovieDetail = async category => {
+  const response = await api.get(`/movie/${category}`, {
     headers: {
       Authorization: `Bearer ${API_KEY}`,
     },
@@ -26,13 +26,13 @@ export const getMovieDetail = async (category) => {
   return response.data;
 };
 
-export const searchMovies = async(query, page) =>{
-  const response = await api.get(`search/movie?page=${page}&query=${query}`,{
+export const searchMovies = async (query, page) => {
+  const response = await api.get(`search/movie?page=${page}&query=${query}`, {
     headers: {
       Authorization: `Bearer ${API_KEY}`,
     },
   });
   return response.data;
-  
+
   // getMovies('search/movie', page, query)
-}
+};
